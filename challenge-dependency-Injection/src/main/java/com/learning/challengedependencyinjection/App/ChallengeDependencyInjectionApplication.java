f5e5ab9cf9c4a5acf5d44b2b890abae5c3c9f728
Luis Fernando <luisfernandomgrs@gmail.com>
2024-10-04 01:34:07 -0300

    Application of solution to challenge

--- challenge-dependency-Injection/src/main/java/com/learning/challengedependencyinjection/App/ChallengeDependencyInjectionApplication.java
+++ challenge-dependency-Injection/src/main/java/com/learning/challengedependencyinjection/App/ChallengeDependencyInjectionApplication.java
@@ -1,11 +1,20 @@
 package com.learning.challengedependencyinjection.App;
 
+import com.learning.challengedependencyinjection.entities.Order;
+import com.learning.challengedependencyinjection.services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.text.Format;
 
 @SpringBootApplication
+@ComponentScan({"com.learning.challengedependencyinjection"})
 public class ChallengeDependencyInjectionApplication implements CommandLineRunner {
+	@Autowired
+	OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeDependencyInjectionApplication.class, args);
@@ -13,6 +22,24 @@ public class ChallengeDependencyInjectionApplication implements CommandLineRunne
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Implementation to use CommandLineRunner");
+		Order order = new Order();
+
+		order.setCode(1034);
+		order.setBasic(150.00);
+		order.setDiscount(20.00);
+		System.out.println("........................................");
+		System.out.println("Pedido código: " + order.getCode() + "\nValor total: R$ " + String.format("%.2f", orderService.total(order)));
+
+		order.setCode(2282);
+		order.setBasic(800.00);
+		order.setDiscount(10.00);
+		System.out.println("........................................");
+		System.out.println("Pedido código: " + order.getCode() + "\nValor total: R$ " + String.format("%.2f", orderService.total(order)));
+
+		order.setCode(1309);
+		order.setBasic(95.90);
+		order.setDiscount(0.0);
+		System.out.println("........................................");
+		System.out.println("Pedido código: " + order.getCode() + "\nValor total: R$ " + String.format("%.2f", orderService.total(order)));
 	}
 }
